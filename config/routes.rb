9a088d24ce1dd39492f3fd00234a249855b01164
Jhonel Villanueva <jonichi16@gmail.com>
2023-06-23 05:47:21 +0800

    feat: Add controller for admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  api_guard_routes for: "admins"
+  api_guard_routes for: "admins", controller: {
+    registration: "admins/registration",
+    authentication: "admins/authentication",
+    passwords: "admins/passwords",
+    tokens: "admins/tokens"
+  }
 end
