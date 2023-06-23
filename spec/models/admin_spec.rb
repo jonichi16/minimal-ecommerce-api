e1fcec986cf9c54ac016c3ecfef87fc830a6e48c
Jhonel Villanueva <jonichi16@gmail.com>
2023-06-23 19:24:13 +0800

    feat: Add test to admin and refresh token model

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
 
 RSpec.describe Admin do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_many(:refresh_tokens).dependent(:delete_all) }
 end
--- spec/models/refresh_token_spec.rb
+++ spec/models/refresh_token_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
 
 RSpec.describe RefreshToken do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:admin) }
 end
